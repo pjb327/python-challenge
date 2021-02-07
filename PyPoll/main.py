dd5fe932e0e6a335ac343ce0b116a0a8bab54c10
Sirke <pjbreyfogle@gmail.com>
2021-02-07 14:16:56 -0600

    beginning

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,16 @@
 # hi
+import os
+import csv
+
+PyPoll_csv = os.path.join("Resources/election_data.csv")
+output = ".Analysis/output.txt"
+
+can_dict = {}
+v_count = 0
+count = 0
+v_percent = 0
+winner = ""
+
+with open(PyPoll_csv, newline = "") as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csv_reader)
