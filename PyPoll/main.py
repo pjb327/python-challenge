6821be5bf6863b43ce56b3c043944b3e010463d1
Sirke <pjbreyfogle@gmail.com>
2021-02-09 18:22:10 -0600

    all zeros but almost

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,13 +18,13 @@ with open(PyPoll_csv, newline = "") as csvfile:
     csv_header = next(csv_reader)
     for x in csv_reader:
         count = count + 1
-        can_dict["Candidate"].append(x[2])
+        # can_dict["Candidate"].append(x[2])
         if x[2] not in can_dict["Candidate"]:
             can_dict["Candidate"].append(x[2])
     for y in can_dict["Candidate"]:
         can_dict["Votes"].append(each_can.count(y))
     for z in can_dict["Votes"]:
-        v_percent = (z/count) * 100
+        v_percent = (z/count)*100
         can_dict["Vote Percent"].append(round(v_percent, 3))
     v_win = max(can_dict["Votes"])
     v_index = can_dict["Votes"].index(v_win)
@@ -36,15 +36,18 @@ with open(PyPoll_csv, newline = "") as csvfile:
         #     can_dict['Votes'].append(1)
 # for i in can_dict['Candidate']:
 #     index = can_dict['Candidate'].index(i)
+
+
 print("Financial Analysis")
 print(f"Total Votes: {count}")
 for a in can_dict["Candidate"]:
     index = can_dict["Candidate"].index(a)
     print(f"{a}: {can_dict['Vote Percent'][index]}% ({can_dict['Votes'][index]})")
+
 print(f"Winning candidate: {winner}")
 
 with open('output.txt', 'w') as text:
     text.write(f"Financial Analysis\nTotal Votes: {count}\nWinning candidate: {winner}")
     for b in can_dict["Candidate"]:
-        bndex = can_dict["Candidate"].index(b)
-        print(f"{b}: {can_dict['Vote Percent'][bndex]}% ({can_dict['Votes'][bndex]})")
+        index = can_dict["Candidate"].index(b)
+        text.write(f"{b}: {can_dict['Vote Percent'][index]}% ({can_dict['Votes'][index]})")
